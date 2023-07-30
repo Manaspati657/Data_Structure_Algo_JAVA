bee0e0d87911ce93b4f1adf11d2b814f3db5b454
Manaspati657 <manaspati657@gmail.com>
2023-07-30 10:31:16 +0530

    prime number commit test

---
+++ 01-math-problems/code/com/mkp/easy/PrimeNumber.java
@@ -0,0 +1,21 @@
+package com.mkp.easy;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+        System.out.println("is Prime 5 :: "+isPrime(2));
+
+    }
+
+    /*
+    * Time complexity : O( sqrt(n))
+    * */
+
+    public static boolean isPrime(int no ){
+        if(no == 0 || no == 1 ) return false;
+        else if(no == 2 || no == 3 ) return true;
+        else if(no % 2 ==0 ) return false;
+        else for (int i = 3; i < Math.sqrt(no); i++) if (no % i == 0) return false;
+        return true;
+    }
+}
