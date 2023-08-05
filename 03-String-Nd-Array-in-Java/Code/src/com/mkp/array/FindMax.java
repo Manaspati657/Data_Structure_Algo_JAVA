8ff6e706cdbc25334be57c2626721379d0a18a20
Manaspati657 <manaspati657@gmail.com>
2023-08-05 22:08:21 +0530

    add a new method maxInRange

--- 03-String-Nd-Array-in-Java/Code/src/com/mkp/array/FindMax.java
+++ 03-String-Nd-Array-in-Java/Code/src/com/mkp/array/FindMax.java
@@ -4,6 +4,7 @@ public class FindMax {
     public static void main(String[] args) {
         int[] arr={232,3,53,33,77,46,96};
         System.out.println(max(arr));
+        System.out.println(maxInRange(arr,2,5));
     }
 
     private static int max(int[] arr) {
@@ -12,4 +13,15 @@ public class FindMax {
             if (arr[i] > max) max = arr[i];
         return max;
     }
+
+    private static int maxInRange(int[] arr , int start,int end) {
+        if(start> end) return -1;
+        if(arr==null) return -1;
+        int max=arr[start];
+        for (int i = start; i < end; i++)
+            if (arr[i] > max) max = arr[i];
+        return max;
+    }
+
+
 }
