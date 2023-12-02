5823e94b571cfc8c85d167e18f47e048321fb59e
Manaspati657 <manaspati657@gmail.com>
2023-12-03 01:03:01 +0530

    lcs problems

---
+++ 18-Dynamic-programming/Code/src/com/mkp/LongestCommonSubSequence/DeleteOperationForTwoStrings583.java
@@ -0,0 +1,28 @@
+package src.com.mkp.LongestCommonSubSequence;
+
+public class DeleteOperationForTwoStrings583 {
+
+    public static void main(String[] args) {
+        String word1 = "sea", word2 = "eat";
+        System.out.println(minDistance(word1,word2));
+    }
+
+    // making word1 and word2 the same nothing but the lcs
+
+    public static int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+//      if we add two string then lcs also includes 2 times so we just substract 2 times lcs from
+//      the total length
+
+        return (m + n) - (2 * dp[m][n]);
+    }
+}
---
+++ 18-Dynamic-programming/Code/src/com/mkp/LongestCommonSubSequence/ShortestCommonSuperSequence_gfg.java
@@ -0,0 +1,48 @@
+package src.com.mkp.LongestCommonSubSequence;
+
+public class ShortestCommonSuperSequence_gfg {
+
+
+    //problem statement:
+    /*
+    *
+    *   SuperSequence means that a string that has both the strings str1 and str2, and all
+    *   the str1 and str2 characters should be in order.
+    *
+    *   e.g. str1="abcd" , str2="wxcd" -> superSequence = "abcdwxcd"
+    *
+    *   Then the Shortest common Super Sequence is we avoid common characters( i.e. Common SubSequence).
+    *   ans -> "abwxcd" -> length is 6 .
+    *
+    *   Solution:
+    *
+    *   for only giving the length we don't have to think anything just mathematics.
+    *   If we don't ignore any common SubSequence then the length of the Super Sequence is
+    *   str1.length()+str2.length() = 8.
+    *   and the longest common sub-sequence length is 2 so formula is
+    *        (str1.length()+str2.length()) - lcs(str1,str2))
+    *
+    * */
+
+    public static void main(String[] args) {
+        String str1="abcd",str2= "xycd";
+        System.out.println(shortestCommonSuperSequence(str1,str2,str1.length(),str2.length()));
+    }
+
+    private static int shortestCommonSuperSequence(String str1, String str2, int m, int n) {
+        return (m+n) - lcs(str1,str2,m,n);
+    }
+    private static int lcs(String str1, String str2, int m, int n) {
+        int[][] dp=new int[m+1][n+1];
+
+        for (int i = 1; i < m+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
+}
